6b397724da7cfb35bdfc381570bf7f1f5bb9922e
keeq0 <myakotnykh1689@gmail.com>
2026-05-07 16:51:44 +0300

    ci: improve CD workflow and add publicPath for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
 })
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/dokkee-front/' : '/',
   devServer: {
     allowedHosts: 'all',
     client: {
